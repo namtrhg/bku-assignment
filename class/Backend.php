d9893063df44b46966983847b49e71492bce4c6a
billdao <bao.dao811@hcmut.edu.vn>
2021-11-21 00:27:47 +0700

    Adding searching based on job name to render product list
    Split header and footer as separate components

--- class/Backend.php
+++ class/Backend.php
@@ -1,16 +1,17 @@
 <?php 
 class Backend
 {
-    private $host = 'localhost';
-    private $user = 'root';
+    // Define all the needed informatio
+    private $host = '';
+    private $user = ''; 
     private $pass = '';
-    private $dbname = 'namkute';
+    private $dbname = '';
     
     private $connect = '';
     
     public function __construct()
     {
-        $this->connect = new mysqli($this->host,$this->user,$this->pass,$this->dbname);
+        $this->connect = new mysqli($this->host,$this->user,$this->pass,$this->dbname,3306);
     }
     
     /*Code o day*/
@@ -30,5 +31,28 @@ class Backend
         return $this->connect->query( "INSERT INTO `jobs` (`Jobs_id`, `user_id`, `Category_id`, `Jobs_name`, `Jobs_description`, `Jobs_salary`, `Jobs_createAt`, `Jobs_updatedAt`, `Jobs_hide`) 
                                                      VALUES (NULL, '1', '".$cate."' , '".$name."', '".$des."', '".$salary."', CURRENT_DATE(), CURRENT_DATE(), '0');");
     }
+
+    public function get_job_by_keyword($keyword)
+    {
+        $sql = "SELECT Jobs_id as 'ID', Jobs_name as 'NAME', Jobs_description as 'DESCRIPTION', Jobs_updatedAt as 'UPDATEDAT', Jobs_salary as 'SALARY' FROM `Jobs` WHERE Jobs_name LIKE ?";
+        if ($stmt = mysqli_prepare($this->connect, $sql)){
+            // Bind variables to the prepared statement as parameters
+            mysqli_stmt_bind_param($stmt, "s", $param_term);
+        
+            // Set parameters
+            $param_term = $keyword . '%';
+            
+            // Attempt to execute the prepared statement
+            if(mysqli_stmt_execute($stmt)){
+                return mysqli_stmt_get_result($stmt);
+            }
+            else
+            {
+                return false;
+            }
+        }
+        else{
+            return false;
+        }
+    }
 }
-?>
